6661ddaa6471dbcac4d0ca6d23ca2d2a07046cb5
Karol <kjampolski@gmail.com>
2021-04-24 16:22:45 +0200

    Changes by Mati

--- src/main/java/com/example/demo/login/LoginController.java
+++ src/main/java/com/example/demo/login/LoginController.java
@@ -9,7 +9,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 
 @RestController
