d7677e061cb56fdb7c35607c27dfba64b2c3be44
Karol <kjampolski@gmail.com>
2021-04-24 19:27:57 +0200

    dgfdgdg

--- src/main/java/com/example/demo/login/UserSecurityConfig.java
+++ src/main/java/com/example/demo/login/UserSecurityConfig.java
@@ -3,11 +3,13 @@ package com.example.demo.login;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @Configuration
 public class UserSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
+    @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
